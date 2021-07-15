318979391d90154d92e1027a7325392f8a7015be
Okamixtape <loup.aubour@gmail.com>
2021-07-15 17:33:29 +0200

    Regex correct°, add confirmation.js

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -1,6 +1,7 @@
 ////////////////////////////////////////////////////////////////////////
 //                                                                    //
 // AFFICHACHE DES TEDDIES STOCKÉS DANS LOCAL STORAGE SUR PANIER.HTML  //
+//             ET FORMULAIRE POUR CONFIRMER COMMANDE                  //
 //                                                                    //
 ////////////////////////////////////////////////////////////////////////
 
@@ -111,7 +112,7 @@ function getStoredTeddies() {
 
         // Fonction expression régulière (REGEX) pour valider prénom, nom et ville
         function validName(value) {
-            return /^([a-zA-Zéèàùôê\s]{3,40}$\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/.test(value)
+            return /^[A-Z-a-zéèàùôê\s]{3,40}$/.test(value)
         }
 
         // Fonction expression régulière (REGEX) pour valider adresse
@@ -126,7 +127,7 @@ function getStoredTeddies() {
 
         // Fonction expression régulière (REGEX) pour valider adresse email
         function validMail(value) {
-            return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(value)
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
         }
 
         // Ajout ligne du formulaire pour le prénom
